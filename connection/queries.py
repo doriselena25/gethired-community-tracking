2fe63f166a65fe93bd28e21477edf90d959239a7
Jose Antonio <josea_458@hotmail.com>
2022-03-03 22:37:30 -0600

    Final changes to the dashboard

--- connection/queries.py
+++ connection/queries.py
@@ -5,21 +5,42 @@ SELECT users.id, creation_at AS sign_up_date , strikes, countries.name AS countr
      JOIN sch_comm.genders ON users.gender_id_id = genders.id 
 """
 
-networking_query = """
-SELECT id, title, number_of_likes
+networking_query_populuar_posts = """
+SELECT title, number_of_likes as likes
 FROM (
 	SELECT discussion_id, COUNT(user_id) AS number_of_likes
-FROM sch_comm.netw_discussion_likes
-GROUP BY discussion_id
+     FROM sch_comm.netw_discussion_likes
+     GROUP BY discussion_id
+     ORDER BY number_of_likes
 ) AS likes_count
 RIGHT JOIN
 sch_comm.netw_discussions AS discussions ON likes_count.discussion_id = discussions.id
 LIMIT 7
 """
 
+networking_query_populuar_categories = """
+SELECT netw_categories.description as category, COUNT(category_id) as posts
+FROM sch_comm.netw_discussions
+JOIN sch_comm.netw_categories ON netw_discussions.category_id = netw_categories.id
+GROUP BY category
+ORDER BY posts DESC
+"""
+
+
+challenges_query = """
+SELECT name as title, description as challenge
+FROM sch_comm.challenges
+WHERE name <> 'Palindrome'
+"""
+
+
 queries_dict = {
      "users" : users_query,
      "users_columns": ['id', 'sign_up_date', 'strikes', 'country', 'country_iso', 'gender'],
-     "networking" : networking_query,
-     "networking_columns" : ['id', 'title', 'number_of_likes']
+     "networking_posts" : networking_query_populuar_posts,
+     "networking_posts_columns" : ['title', 'likes'],
+     "networking_categories" : networking_query_populuar_categories,
+     "networking_categories_columns" : ['category', 'posts'],
+     "challenges": challenges_query,
+     "challenges_columns": ['title', 'challenge']
 }
--- dashboard.py
+++ dashboard.py
@@ -12,21 +12,23 @@ import connection.connection as connection
 st.set_page_config(layout="wide")
 
 st.title("""
-GetHired Community dashboard
+GetHired Community Dashboard
 """)
 
 #Sidebar configuration
 
 date_min = st.sidebar.date_input(
      "Select initial date",
-     datetime.date(2019, 7, 6))
+     datetime.date(2021, 12, 1))
 date_max = st.sidebar.date_input(
      "Select End date",
-     datetime.date(2019, 7, 6))
+     datetime.date(2023, 1, 1))
 
 st.write('Range from: ' + str(date_min) + ' to ' + str(date_max) )
 
+
 #SQL connection data
+
 #Parameters
 params_dict = {
     "database" : st.secrets.postgres.database,
@@ -39,36 +41,41 @@ params_dict = {
 
 #Connection to the DB
 connection1 = connection.connect(params_dict)
+
+#Info about users 
 users_df = connection.postgresql_to_dataframe(connection1, queries.queries_dict['users'], queries.queries_dict['users_columns'])
 users_df['sign_up_date'] = pd.to_datetime(users_df['sign_up_date'])
 
+
+
+#Get the data from a CSV
+df_users  = pd.read_csv('data/USERS_MOCK_DATA_1.csv')
+
+
+#Users per month analys
 created_by_2 = users_df.groupby(pd.Grouper(key='sign_up_date',freq='M'))['id'].count()
 created_by_indexes_2 = created_by_2.index
+
+st.subheader('Users analysis')
 
 st.subheader('New Users per month')
 st.line_chart(data=created_by_2)
 
 
-#Get the data
-df_users  = pd.read_csv('data/USERS_MOCK_DATA_1.csv')
-df_users['creado_dia'] = pd.to_datetime(df_users['creado_dia'])
-
-challenges_df = pd.read_csv('data/dummy_data.csv')
-
-
 #Column adjustment
 col1, col2 = st.columns([3, 1])
-col3, col4  = st.columns(2)
+#col3, col4  = st.columns(2)
 
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
 
-
-#Users per month analys
-
-created_by = df_users.groupby(pd.Grouper(key='creado_dia',freq='M'))['id'].count()
-created_by_indexes = created_by.index
-
-col1.subheader('New Users per month')
-col1.line_chart(data=created_by)
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
 
 #Analysis per country 
@@ -78,32 +85,48 @@ countries_count_sorted = countries_count.sort_values( ascending=False)
 countries_count_countries_name = countries_count_sorted.index
 
 
-col2.subheader('Countries with most users')
-col2.write(countries_count_sorted.head(6))
-
-
-#Challenges and community analysis 
-
-popular_post_count = challenges_df.groupby('discussion-post')['likes'].sum()
-col3.subheader('Popular posts')
-col3.write(popular_post_count)
-
-
-second_chart = alt.Chart(challenges_df).mark_bar().encode(
-    x='Challenges completados ', 
-    y='Tema challenges'
-)
-
-col4.subheader('Popular topics')
-col4.altair_chart(second_chart, use_container_width=True)
+col1.subheader('Countries with most users')
+col1.bar_chart(countries_count_sorted.head(6))
 
 
 #Gender analysis 
 
 df_gender = pd.read_csv('data/gender_mock_data.csv')
 gender_analysis = (df_gender.groupby('genero').count() / df_gender.groupby('genero').count().sum()) * 100
-st.write('Gender')
-st.bar_chart(data=gender_analysis)
+col2.subheader('Gender analysis')
+col2.bar_chart(data=gender_analysis)
+
+
+#Challenges and community analysis 
+
+
+
+st.subheader('Community analysis')
+col3, col4  = st.columns(2)
+
+#popular_post_count = challenges_df.groupby('discussion-post')['likes'].sum()
+popular_post_count = connection.postgresql_to_dataframe(connection1, queries.queries_dict['networking_posts'], queries.queries_dict['networking_posts_columns'])
+col3.subheader('Popular posts')
+col3.table(popular_post_count)
+
+
+#Networking categories analysis
+
+categories_count = connection.postgresql_to_dataframe(connection1, queries.queries_dict['networking_categories'], queries.queries_dict['networking_categories_columns'])
+col4.subheader('Popular categories')
+col4.table(categories_count)
+
+
+
+
+#Challenges analysis 
+
+st.subheader('Challenges analysis')
+
+challenges_df = connection.postgresql_to_dataframe(connection1, queries.queries_dict['challenges'], queries.queries_dict['challenges_columns'])
+st.subheader('Popular challenges')
+st.table(challenges_df)
+
 
 
 
